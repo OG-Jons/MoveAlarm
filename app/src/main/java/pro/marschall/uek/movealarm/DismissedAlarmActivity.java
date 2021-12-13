f822b5517c49ee03f26fe5aa6d0645f4c2c8d2b2
OG-Jons <jonas@marschall.pro>
2021-12-13 16:17:56 +0100

    Show the "dismissed alarm" screen for 5 seconds, then go back to home

--- app/src/main/java/pro/marschall/uek/movealarm/DismissedAlarmActivity.java
+++ app/src/main/java/pro/marschall/uek/movealarm/DismissedAlarmActivity.java
@@ -2,7 +2,9 @@ package pro.marschall.uek.movealarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class DismissedAlarmActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class DismissedAlarmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dismissed_alarm);
+
+        // wait 5 seconds and then close the app
+        System.out.println("yes");
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            System.out.println("waited 5 secs");
+            closeApp();
+        }, 5000);
+    }
+
+    private void closeApp() {
+        System.out.println("Closing app...");
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        startActivity(startMain);
     }
 }
