b7e597b02873785f467db31412744a765b1c801a
OG-Jons <jonas@marschall.pro>
2021-12-13 13:07:43 +0100

    created model for the Alarms

---
+++ app/src/main/java/pro/marschall/uek/movealarm/models/AlarmModel.java
@@ -0,0 +1,39 @@
+package pro.marschall.uek.movealarm.models;
+
+import java.time.LocalTime;
+
+public class AlarmModel {
+    LocalTime time;
+    boolean active;
+    String dismissType;
+
+    public AlarmModel(LocalTime time, boolean active, String dismissType) {
+        this.time = time;
+        this.active = active;
+        this.dismissType = dismissType;
+    }
+
+    public LocalTime getTime() {
+        return this.time;
+    }
+
+    public boolean isActive() {
+        return this.active;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getDismissType() {
+        return dismissType;
+    }
+
+    public void setDismissType(String dismissType) {
+        this.dismissType = dismissType;
+    }
+}
