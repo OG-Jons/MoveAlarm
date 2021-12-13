00175284384875e234ba8d3a42288f4d21fcd2c0
OG-Jons <jonas@marschall.pro>
2021-12-13 08:35:12 +0100

    init commit

---
+++ app/src/main/java/pro/marschall/uek/movealarm/DismissAlarmMathActivity.java
@@ -0,0 +1,14 @@
+package pro.marschall.uek.movealarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DismissAlarmMathActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dismiss_alarm_math_activty);
+    }
+}
